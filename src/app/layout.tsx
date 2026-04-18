f4163e706e2579fe61cbdd76b6b720e4b71bd8e7
Afnan-Ahmad961 <afnun659@gmail.com>
2026-04-18 18:44:53 +0500

    Update meta-pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,24 +16,24 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       <head>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1246261310921568');
-            fbq('track', 'PageView');
-          `}
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2013989605819671');
+fbq('track', 'PageView');
+            `}
         </Script>
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1246261310921568&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2013989605819671&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
