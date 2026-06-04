edfc90a9f06825cbc1778fb2b9cbdec5c9ae896e
Afnan Ahmad <afnun659@gmail.com>
2026-06-04 16:28:18 +0500

    pixel code update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '946641981329100');
+fbq('init', '1739716333682081');
 fbq('track', 'PageView');
             `}
         </Script>
@@ -33,7 +33,7 @@ fbq('track', 'PageView');
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=946641981329100&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1739716333682081&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
